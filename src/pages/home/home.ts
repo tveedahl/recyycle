4733c95bb648cfe5a73c5e0c01af71ac18bba75c
Tim Veedahl <tveedahl@gmail.com>
2017-05-09 10:25:31 -0500

    Addition home slide updates

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { SignupPage } from '../pages/signup/signup';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+constructor(public navCtrl: NavController) {
 
   }
 
+}
+
+$scope.$on("$ionicView.enter", function(event, data){
+   alert('asswipe!');
+    // handle event
+   console.log("State Params: ", data.stateParams);
+});
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
+function navToSignup () {
+    wait(3000);
 }
