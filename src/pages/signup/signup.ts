ea9906bab8f46dbbc080c5557566e8c859175770
Tim Veedahl <tveedahl@gmail.com>
2017-05-12 13:55:13 -0500

    Got signup alert working!

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 ﻿import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from 'ionic-angular';
 import firebase from 'firebase';
 
 /**
@@ -18,7 +19,7 @@ export class Signup {
 
   private signupForm : FormGroup;
  
-  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public alertCtrl: AlertController) {
     this.signupForm = formBuilder.group({
       email: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
       password: ['']
@@ -31,11 +32,14 @@ export class Signup {
   signupUser() {
     //console.log(this.signupForm.value);
     firebase.auth().createUserWithEmailAndPassword(this.signupForm.value.email, this.signupForm.value.password).catch(function(error) {
-      // Handle Errors here.
       //var errorCode = error.code;
       //var errorMessage = error.message;
-      // ...
     });
-
+    let alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 }
